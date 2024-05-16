b06bdd7fb353bbcfafc26c16481dc68793554707
DESKTOP-JPJDCF9\Carlos <limaantoniocarlos20@gmail.com>
2024-05-15 21:30:34 -0300

    [fix]: env

--- ormconfig.js
+++ ormconfig.js
@@ -1,21 +1,12 @@
 const dotevnt = require("dotenv");
 
 dotevnt.config({
-  path: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.production",
+  path: process.env.NODE_ENV == "dev " ? ".env.dev" : ".env.production",
 });
-
-console.log(process.env.TYPEORM_MIGRATION);
-console.log(process.env.TYPEORM_ENTITIES);
-console.log(process.env.TYPEORM_MIGRATION_DIR);
-console.log(process.env.NODE_ENV);
 
 module.exports = {
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: 'mypass',
-  database: "db_my_account",
+  url: process.env.DATABASE_URL,
   migrations: [process.env.TYPEORM_MIGRATION],
   entities: [process.env.TYPEORM_ENTITIES],
   cli: {
